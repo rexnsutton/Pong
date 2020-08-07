b01e96e93b4e7a03a28f963475c3869cf26fe879
Student Name <rexsutton2@gmail.cogit>
2020-08-07 12:07:18 -0500

    set up renderer class and added jframe

---
+++ src/pong/Renderer.java
@@ -0,0 +1,15 @@
+package pong;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Renderer extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Pong.pong.render(g);
+    }
+}
